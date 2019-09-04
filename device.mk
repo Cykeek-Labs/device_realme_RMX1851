bcb83ccf679c305899b11d4811a0c163a66afb62
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    RMX1851: Set soong namespace outside of boardconfig
    Change-Id: I86208dd2d706c6a3937ca22c2e9b878d65b5f974

--- device.mk
+++ device.mk
@@ -325,6 +325,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf::$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \

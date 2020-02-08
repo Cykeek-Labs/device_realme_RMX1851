504e93ff26714f0a8d2dac3a07aa5d2e9122e934
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-08 22:15:58 +0530

    RMX1851: Doze: Allow to wake the screen instead of pulsing
     * Highly inspired by iOS raise to wake
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I832fd03e9538a507ec9b3ee7cc384bd71d6efbdd

--- parts/src/org/lineageos/settings/doze/ProximitySensor.java
+++ parts/src/org/lineageos/settings/doze/ProximitySensor.java
@@ -22,6 +22,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
@@ -35,6 +38,7 @@ public class ProximitySensor implements SensorEventListener {
 
     // Maximum time for the hand to cover the sensor: 1s
     private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+    private static final int WAKELOCK_TIMEOUT_MS = 300;
 
     // Minimum time until the device is considered to have been in the pocket: 2s
     private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
@@ -43,6 +47,8 @@ public class ProximitySensor implements SensorEventListener {
     private Sensor mSensor;
     private Context mContext;
     private ExecutorService mExecutorService;
+    private PowerManager mPowerManager;
+    private WakeLock mWakeLock;
 
     private boolean mSawNear = false;
     private long mInPocketTime = 0;
@@ -51,6 +57,8 @@ public class ProximitySensor implements SensorEventListener {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mPowerManager = mContext.getSystemService(PowerManager.class);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
@@ -60,10 +68,17 @@ public class ProximitySensor implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        boolean isRaiseToWake = DozeUtils.isRaiseToWakeEnabled(mContext);
         boolean isNear = event.values[0] < mSensor.getMaximumRange();
         if (mSawNear && !isNear) {
             if (shouldPulse(event.timestamp)) {
-                DozeUtils.launchDozePulse(mContext);
+                if (isRaiseToWake) {
+                    mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
+                    mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                        PowerManager.WAKE_REASON_GESTURE, TAG);
+                } else {
+                    DozeUtils.launchDozePulse(mContext);
+                }
             }
         } else {
             mInPocketTime = event.timestamp;

b54fe113787c8145e8c07cdeff04d57e261b9f64
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-12 23:54:39 +0900

    RMX1851: Optimize everything on preopt
    We have plenty of space left at /system
    
    Change-Id: I39d552259b66ec329c56cd4f4a7fc46fb2c07457
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -188,6 +188,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service:64 \

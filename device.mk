e3faa327c7060ba622a53f859454c63f5c4bb016
Scott Warner <Tortel1210@gmail.com>
2020-06-24 12:38:56 -0400

    RMX1851: Use opensource btconfigstore
    Change-Id: Id16333c0b36fbf93e504fe6cb0107fce88721791

--- device.mk
+++ device.mk
@@ -137,7 +137,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-impl \
-    libbthost_if
+    libbthost_if \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \

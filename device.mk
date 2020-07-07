535c7a7189735cf9a9bda60fdf0e4b22df6c891d
karthick111 <karthickrko61@gmail.com>
2020-07-07 21:10:18 +0530

    RMX1851: Use 6GB dalvik config
    Change-Id: I51a62baa6bbc38bc9e6bfc45de4834207ebff3e4

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/realme/RMX1851/RMX1851-vendor.mk)

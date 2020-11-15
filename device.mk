e6fd61e205889e806de3ddece867021ba4903946
karthick111 <karthickrko61@gmail.com>
2020-11-15 12:50:05 +0530

    RMX1851: Use prebuilt hwcomposer from C.07
     * Also set BUILD_BROKEN_DUP_RULES=true
    
    Change-Id: I1f3f431796291d8aa85df98180ebe6dcd83e771d

--- device.mk
+++ device.mk
@@ -196,7 +196,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm710 \
-    hwcomposer.sdm710 \
     libtinyxml \
     libvulkan \
     memtrack.sdm710 \

81c3a2291eb64971aab7c0db4dcd24a466589309
Bruno Martins <bgcngm@gmail.com>
2020-11-23 23:16:40 +0000

    RMX1851: sepolicy: Add /product file context for dpmd daemon
    This got removed from QCOM sepolicies because DPM was
    moved from /product to /system_ext.
    
    Change-Id: Ifdff048d8b68bd9593e9a30394888a966c198f8d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,6 +175,7 @@ VENDOR_SECURITY_PATCH := 2020-09-05
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # System-as-root

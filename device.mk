013910596fa795804df8e17752fa851ca4204aff
karthick111 <karthickrko61@gmail.com>
2021-04-24 14:09:58 +0530

    RMX1851: Import powerhint.json from Google bonito
    Change-Id: I2b2a3301bfd357655133d8e9c375dcc71d7fb06e

--- device.mk
+++ device.mk
@@ -305,6 +305,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Preopt SystemUI

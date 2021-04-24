b0f59f8cb0142edbb37e2d3bccc1dfedffc9108e
karthick111 <karthickrko61@gmail.com>
2021-04-24 20:56:29 +0530

    RMX1851: Update vendor security patch level
    Change-Id: I5943e177d4ff3439077d815965be3917ba061540

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-09-05
+VENDOR_SECURITY_PATCH := 2021-03-05
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk

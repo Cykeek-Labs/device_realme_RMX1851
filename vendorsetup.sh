0073490fa9cf1676521bcf3e286960bba7188c0e
Cykeek <Cykeek@proton.me>
2023-04-27 11:48:53 +0530

    RMX1851: Clone dynamic branch instead of default
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,7 +17,7 @@ banner 2>1
 banner 2>1
 echo "Cloning Kernel..."
 git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang
-git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm710
+git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 -b dynamic kernel/realme/sdm710
 
 
 # RealmeParts

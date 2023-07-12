e2d63655f73c0f88f251030a80f94d7ffbe90280
Pascoato <viniciuspascoato@gmail.com>
2023-07-12 09:40:03 +0530

    RMX1851: Drop libhidl & build vendor variant
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -276,8 +276,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system \
     android.hidl.manager@1.0_vendor \
     libhidltransport \

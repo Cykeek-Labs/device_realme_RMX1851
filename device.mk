40697b07d53a7aebdbc292ffd2e7c290c0c0031e
Santhosh Behara <santhoshbehara@codeaurora.org>
2023-09-02 15:39:28 +0530

    RMX1851:  media: Add and publish Codec2 HAL service *Add 2 codec2 relates libs to PRODUCT_PACKAGES in order to copy them to /vendor/lib/ *Add seccomp policies for codec2 services
    Change-Id: I83af4f6b80886ca18aa059285e0b94403c027e29
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -449,6 +449,7 @@ PRODUCT_PACKAGES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp/mediaswcodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy

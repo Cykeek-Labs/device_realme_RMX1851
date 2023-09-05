1b46cb5f660a62c6192654009ef4229e187bf27a
sureshfizzy <sureshfizzy0503@gmail.com>
2023-09-05 12:18:04 +0530

    RMX1851: Import OnePlus Dolby Co-authored-by: Anushek Prasal <anushekprasal@gmail.com> Co-authored-by: HELLBOY017 <abhaygill017@gmail.com> Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev> Co-authored-by: ziasam <ziauddinsameer@gmail.com> Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com> Co-authored-by: sureshfizzy <sureshfizzy0503@gmail.com> Co-authored-by: Niranjan BR <niranjankannan2003@gmail.com> Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -235,6 +235,16 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service:64 \
     android.hardware.graphics.mapper@2.0-impl-qti-display
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/hiddenapi-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxservice.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \

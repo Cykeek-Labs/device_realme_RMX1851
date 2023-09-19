69f027a9ce8466e32f00b1ee519975001b47f442
Cykeek <Cykeek@proton.me>
2023-09-19 23:11:41 +0530

    RMX1851: vendorsetup: Download files in new way
    - Added predefined folder location it search for those folder
    if exists then download from repo otherwise skip the execution
    and build.
    
    - This will skip the download repository part which anyway
    consumes time while checking it.
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,13 +13,26 @@ echo "================================"
 
 banner 2>1
 
-# Kernel
-banner 2>1
-echo "Cloning Kernel..."
-git clone https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm710
+# Define the folder names and their corresponding download URLs
+folders=(
+    "packages/apps/RealmeParts=https://github.com/Cykeek-Labs/packages_apps_realmeparts"
+    "prebuilts/clang/host/linux-x86/clang-playground=https://gitlab.com/PixelOS-Devices/playgroundtc.git"
+    "kernel/realme/sdm710=https://github.com/Cykeek-Labs/kernel_realme_sdm710"
+)
 
-# RealmeParts
-banner 2>1
-echo "Cloning RealmeParts..."
-git clone https://github.com/Cykeek-Labs/packages_apps_realmeparts packages/apps/RealmeParts
+# Loop through the folder names and URLs
+for folder_info in "${folders[@]}"; do
+    # Split folder_info into folder_name and download_url
+    IFS="=" read -r folder_name download_url <<< "$folder_info"
+
+    # Check if the folder exists
+    if [ -d "$folder_name" ]; then
+        echo "Folder '$folder_name' already exists. No need to download."
+    else
+        echo "Folder '$folder_name' doesn't exist. Downloading..."
+        # You can replace the echo statement below with your actual download command.
+        git clone $download_url $folder_name
+        # Uncomment the line below and replace with your actual download command.
+        # git clone "$download_url" "$folder_name"
+    fi
+done

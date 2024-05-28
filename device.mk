1cbfab6f8a86b95925c4724a4965b6f29c37754c
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    RMX1851: Allow to access bootable/deprecated-ota targets
    RMX1921 is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to device namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -451,6 +451,7 @@ endif
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     vendor/qcom/opensource/usb/etc \
     hardware/qcom-caf/sdm845 \
     hardware/google/interfaces \
